536cce69bc2bafe76dbbaafa72647ad0744cacd0
Antoine07 <antoine.lucsko@gmail.com>
2024-11-26 10:38:27 +0100

    load data

---
+++ scripts/load/load_to_hadoop.py
@@ -0,0 +1,8 @@
+from scripts.load.storm_events import load_storm_events_to_hadoop
+from scripts.load.gsod import load_gsod_to_hadoop
+from hdfs import InsecureClient
+
+hdfs_client = InsecureClient('http://localhost:9870', user='root')
+
+load_storm_events_to_hadoop(hdfs_client)
+load_gsod_to_hadoop(hdfs_client)
